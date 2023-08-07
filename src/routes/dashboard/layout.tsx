6b3f47dd7e3577929a6a3f14ed4299681f58cb55
Joel M. Boada <jmboada30@gmail.com>
2023-08-06 23:50:43 -0400

    add validation jwt using cookie in dashboardLayout

--- src/routes/dashboard/layout.tsx
+++ src/routes/dashboard/layout.tsx
@@ -1,5 +1,14 @@
 import { Slot, component$ } from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
 import Navbar from '~/components/shared/navbar/navbar';
+
+export const useCheckAuthCookie = routeLoader$(({cookie, redirect})=> {
+  const jwtCookie = cookie.get('jwt');
+  console.log('jwtCookie', jwtCookie);
+  if (!jwtCookie) throw redirect(302, '/login');
+  if(jwtCookie.value !== 'esto_es_un_token') throw redirect(302, '/login');
+  
+});
 
 export default component$(() => {
   return (
